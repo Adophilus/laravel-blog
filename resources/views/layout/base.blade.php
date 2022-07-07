88719ebf10e6d562ff1cf46ac850ca1397f0cb21
Adophilus <uchenna19of@gmail.com>
2022-07-07 13:55:01 +0100

    done with first blog post

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -6,6 +6,7 @@
 	<title>@yield("title") | Alive</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+	<link rel="stylesheet" type="text/css" href="/css/hint.min.css">
 	<script type="text/javascript" src="{{asset('js/alpine.js')}}" defer></script>
 </head>
 <body class="bg-gray-100 font-lato selection:bg-primary">
