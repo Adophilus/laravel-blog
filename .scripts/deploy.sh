8ea877115ab6ddf5067b34dc438e70e62ec6c4ea
Adophilus <uchenna19of@gmail.com>
2022-07-18 08:59:29 +0100

    added deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,10 +5,10 @@ set -e
 echo "Deploying application ..."
  
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down || true
     # Update codebase
-    git fetch origin deploy
-    git reset --hard origin/deploy
+    git fetch origin main
+    git reset --hard origin/main
  
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader
