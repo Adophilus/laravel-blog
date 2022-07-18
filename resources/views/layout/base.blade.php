7b44b20f26fa6960cdaa412f7c206687d1b98107
Adophilus <uchenna19of@gmail.com>
2022-07-18 10:07:22 +0100

    updated the assets url

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -5,9 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>@yield("title") | Alive</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
 	<link rel="stylesheet" type="text/css" href="/css/hint.min.css">
-	<script type="text/javascript" src="{{asset('js/alpine.js')}}" defer></script>
+	<script type="text/javascript" src="/js/alpine.js" defer></script>
 </head>
 <body class="bg-gray-100 font-lato selection:bg-primary">
 	@include("inc.navbar")
